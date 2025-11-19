ce9c65a43db1dcf93287bff40a2bdf8ee20fd255
BragaSoterasVinicius <viniciussoteras@gmail.com>
2025-11-18 23:36:11 -0300

    fix(DatabaseConfig): settado @ApplicationScoped pra instanciar o repo

--- src/main/java/com/economy/infrastructure/config/DatabaseConfig.java
+++ src/main/java/com/economy/infrastructure/config/DatabaseConfig.java
@@ -12,27 +12,27 @@ public class DatabaseConfig {
     public DatabaseConnection databaseConnection(AgroalDataSource dataSource) {
         return new DatabaseConnectionImpl(dataSource);
     }
-
+    @ApplicationScoped
     public DepositoRepository depositoRepository(DatabaseConnection databaseConnection) {
         return new JdbcDepositoRepository(databaseConnection);
     }
-
+    @ApplicationScoped
     public EmpregadoRepository empregadoRepository(DatabaseConnection databaseConnection) {
         return new JdbcEmpregadoRepository(databaseConnection);
     }
-
+    @ApplicationScoped
     public EmpresaRepository empresaRepository(DatabaseConnection databaseConnection) {
         return new JdbcEmpresaRepository(databaseConnection);
     }
-
+    @ApplicationScoped
     public PagamentoRepository pagamentoRepository(DatabaseConnection databaseConnection) {
         return new JdbcPagamentoRepository(databaseConnection);
     }
-
+    @ApplicationScoped
     public PropostaRepository propostaRepository(DatabaseConnection databaseConnection) {
         return new JdbcPropostaRepository(databaseConnection);
     }
-
+    @ApplicationScoped
     public SaqueRepository saqueRepository(DatabaseConnection databaseConnection) {
         return new JdbcSaqueRepository(databaseConnection);
     }
