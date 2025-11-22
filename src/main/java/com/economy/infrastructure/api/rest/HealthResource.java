0a84679d179c238f9d37b0826bb6be894224f5ab
BragaSoterasVinicius <viniciussoteras@gmail.com>
2025-11-22 15:56:45 -0300

    ainda tentando resolver a questao do render

--- src/main/java/com/economy/infrastructure/api/rest/HealthResource.java
+++ src/main/java/com/economy/infrastructure/api/rest/HealthResource.java
@@ -2,11 +2,11 @@ package com.economy.infrastructure.api.rest;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 
-@Path("/")
+@Path("/health")
 public class HealthResource {
     //Classe feita para lidar com um erro do render
     @GET
-    public String home() {
-        return "eco-nomy API is running";
+    public String health() {
+        return "OK";
     }
 }
